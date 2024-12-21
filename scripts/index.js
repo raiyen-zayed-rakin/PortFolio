fe760a9d61cf8e36656e6103880df61dd463d683
Raiyen Zayed Rakin <rakinaiub2023@gmail.com>
2024-12-21 07:51:08 +0600

    Need to redo the entire thing

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+function downloadCV(){
+        var pdfWindow = window.open("assets/Rakin_CV.pdf", "_blank");
+
+        // After opening, change the title of the new window (if possible)
+        pdfWindow.document.title = "Rakin's PDF";
+}
