e008a49db40bc946b35b0e5eb7e54d68a88b4966
Raiyen Zayed Rakin <rakinaiub2023@gmail.com>
2025-04-04 10:37:13 +0600

    new

--- old/scripts/index.js
+++
@@ -1,6 +0,0 @@
-function downloadCV(){
-        var pdfWindow = window.open("assets/Rakin_CV.pdf", "_blank");
-
-        // After opening, change the title of the new window (if possible)
-        pdfWindow.document.title = "Rakin's PDF";
-}
