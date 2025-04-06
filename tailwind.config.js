c53e2bca7040f7d542c178b099df86a93746c4e0
Raiyen Zayed Rakin <rakinaiub2023@gmail.com>
2025-04-06 22:50:29 +0600

    added fade-in animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,10 +14,15 @@ module.exports = {
           '0%': { opacity: '0', transform: 'translateX(60px)', filter: 'blur(4px)' },
           '100%': { opacity: '1', transform: 'translateX(0)', filter: 'blur(0)' },
         },
+        'fade-in-bottom': {
+          '0%': { opacity: '0', transform: 'translateY(60px)', filter: 'blur(4px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)', filter: 'blur(0)' },
+        },
       },
       animation: {
         'fade-in-left': 'fade-in-left 2s ease-out forwards',
         'fade-in-right': 'fade-in-right 2s ease-out forwards',
+        'fade-in-bottom': 'fade-in-bottom 1s ease-out forwards',
       },
     },
   },
