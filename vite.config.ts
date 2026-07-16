3d9c2c704470c259fa1e30bd609e8e368e98bd6a
Raiyen Zayed Rakin <rakinaiub2023@gmail.com>
2026-07-16 11:18:06 +0600

    setup2

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/PortFolio/",
   plugins: [react(), tailwindcss()],
 });
